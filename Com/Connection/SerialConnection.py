7efc28e96e99603b69230cafbcb19d9d38127c98
ermu1013 <ermu1013@gmail.com>
2023-04-11 16:48:41 +0200

    KW14-2 add a GUI element to pick the connection comport

--- Com/Connection/SerialConnection.py
+++ Com/Connection/SerialConnection.py
@@ -2,6 +2,7 @@ from typing import TypeAlias, Callable, Optional
 
 import serial.tools.list_ports
 from serial import Serial
+from serial.tools.list_ports_common import ListPortInfo
 
 from RoboControl.Com.Connection.Connection import Connection
 from RoboControl.Com.Ascii.DataPacketAscii import DataPacketAscii
@@ -13,6 +14,7 @@ Listener: TypeAlias = [Callable or any]
 
 
 class SerialConnection(Connection):
+    port: str = "COM4"
 
     def __init__(self):
         super().__init__()
@@ -21,15 +23,22 @@ class SerialConnection(Connection):
         self._serial_stream: Optional[Serial] = None
 
     def connect(self, data_packet_receiver: Listener) -> None:
-        ports = list(serial.tools.list_ports.comports())
-
-        for p in ports:
-            print(p[1])
-
-        port = "COM3"
-
-        self._serial_stream = serial.Serial(port, 115200)
-
+        self._serial_stream = serial.Serial(self.port, 115200)
         self._data_output = AsciiOutput(self._serial_stream)
         self._data_input = AsciiInput(self._serial_stream)
         super().connect(data_packet_receiver)
+    
+    def disconnect(self) -> None:
+        super().disconnect()
+        self._serial_stream.close()
+
+    @staticmethod
+    def get_ports() -> list[ListPortInfo]:
+        return list(serial.tools.list_ports.comports())
+
+    def set_port(self, port: str) -> "SerialConnection":
+        available_ports = [p.name for p in self.get_ports()]
+        if port not in available_ports:
+            raise ValueError(f"Port '{port}' is invalid. Please pick from: {available_ports}")
+        self.port = port
+        return self

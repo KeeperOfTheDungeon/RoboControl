86f6251f644a1bd4745938a7575b701a8a7ce7a8
koda1026 <koda1026@h-ka.de>
2023-08-04 16:01:00 +0200

    changed PicoConnection to add pico in and outputs.

--- Com/Connection/PicoConnection.py
+++ Com/Connection/PicoConnection.py
@@ -2,45 +2,26 @@ from RoboControl.Com.Pico.PicoInput import PicoInput
 from RoboControl.Com.Pico.PicoOutput import PicoOutput
 from RoboControl.Com.Connection.Connection import Connection
 
-# from RoboControl.Com.Ascii.DataPacketAscii import DataPacketAscii
-
 
 class PicoConnection(Connection):
- #   port: str = "COM4"
+    connected: bool = False
 
-  #  open_streams: Dict[str, Optional[Serial]] = {}
+    open_streams: Dict[str, Optional[Serial]] = {}
 
     def __init__(self):
         super().__init__()
+
         # clear programs form pio for clean restart
         rp2.PIO(0).remove_program()
         rp2.PIO(1).remove_program()
-"""
-        # FIXME this isn't really optional is it
-        self._serial_stream: Optional[Serial] = None
 
     def connect(self, data_packet_receiver: Listener) -> None:
-        if not self.open_streams.get(self.port):
-            SerialConnection.open_streams[self.port] = serial.Serial(self.port, 115200)
-        self._serial_stream = SerialConnection.open_streams[self.port]
-
-        self._data_output = AsciiOutput(self._serial_stream)
-        self._data_input = AsciiInput(self._serial_stream)
-        super().connect(data_packet_receiver)
+        if not self.connected:
+            self._data_output = PicoInput() # add data_output
+            self._data_input = PicoOutput # add data_input
+            super().connect(data_packet_receiver)
 
     def disconnect(self) -> None:
-        SerialConnection.open_streams[self.port] = None
+        self._data_input.stop()
+        self._data_output.stop()
         super().disconnect()
-        self._serial_stream.close()
-
-    @staticmethod
-    def get_ports() -> list[ListPortInfo]:
-        return list(serial.tools.list_ports.comports())
-
-    def set_port(self, port: str) -> "SerialConnection":
-        available_ports = [p.name for p in self.get_ports()]
-        if port not in available_ports:
-            raise ValueError(f"Port '{port}' is invalid. Please pick from: {available_ports}")
-        self.port = port
-        return self
-"""

915101ff78586add6ddc747b12deeaf763e261fe
ermu1013 <ermu1013@gmail.com>
2023-09-17 22:25:15 +0200

    conflict: reformat various files

--- Robot/Device/remoteProcessor/RemoteProcessorList.py
+++ Robot/Device/remoteProcessor/RemoteProcessorList.py
@@ -1,12 +1,9 @@
 
 class RemoteProcerssorList(list):
-
     def find_on_id(self, id):
-        print("RPL : looking for id - ",id)
+        # print("RPL : looking for id - ",id)
         for processor in self:
-            print("RPL : ",processor, " id : ", processor.get_remote_id())
+            # print("RPL : ",processor, " id : ", processor.get_remote_id())
             if processor.has_remote_id(id):
                 return processor
-
         return None
-

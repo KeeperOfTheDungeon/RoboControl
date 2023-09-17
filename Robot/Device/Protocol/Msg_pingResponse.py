48bd33a1ae5f61a5364f9c6d52cfb6098135e210
ermu1013 <ermu1013@gmail.com>
2023-09-17 22:33:56 +0200

    conflict: refactor various files

--- Robot/Device/Protocol/Msg_pingResponse.py
+++ Robot/Device/Protocol/Msg_pingResponse.py
@@ -7,8 +7,8 @@ INDEX_TTL = 0
 
 class Msg_pingResponse(RemoteMessage):
 
-    def __init__(self):
-        super().__init__(DeviceProtocol.MSG_PING_RESPONSE, "msgpingResponse", "response to a ping command")
+    def __init__(self, id: int = DeviceProtocol.MSG_PING_RESPONSE):
+        super().__init__(id, "msgpingResponse", "response to a ping command")
         self._ttl_index = 0
         self._parameter_list.append(RemoteParameterUint8("ttl", "time to live"))
 

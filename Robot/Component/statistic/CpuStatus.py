dbd3c89cc3379caeed4ffc73472f9d4b0030fbea
ermu1013 <ermu1013@gmail.com>
2023-09-17 22:25:08 +0200

    reformat various files

--- Robot/Component/statistic/CpuStatus.py
+++ Robot/Component/statistic/CpuStatus.py
@@ -2,46 +2,39 @@ from RoboControl.Robot.Component.statistic.DeviceStatus import DeviceStatus
 
 
 class CpuStatus(DeviceStatus):
+    _SYSTEM_STATE_NAMES = {"init", "running", "stopped", "selftest", "standby"}
 
-	_SYSTEM_STATE_NAMES  = {"init","running","stopped","selftest","standby"}
+    #	private SystemState_e systemState=SystemState_e.RUNNING;
 
-#	private SystemState_e systemState=SystemState_e.RUNNING;
+    def __init__(self):
+        super().__init__()
+        self._min_load = 0
+        self._max_load = 0
+        self._last_load = 0
+        self._status_listener = list()
 
+    def get_min_load(self):
+        return self._min_load
 
-	def __init__(self):
-		super().__init__()
-		self._min_load = 0
-		self._max_load = 0
-		self._last_load = 0
-		self._status_listener = list()
+    def get_max_load(self):
+        return self._max_load
 
-	def get_min_load(self):
-		return self._min_load
+    def get_last_load(self):
+        return self._last_load
 
+    def add_status_listener(self, listener):
+        self._status_listener.append(listener)
 
-	def get_max_load(self):
-		return self._max_load
+    def remove_status_listener(self, listener):
+        self._status_listener.remove(listener)
 
+    def process_cpu_status_message(self, remote_data):
+        self._min_load = remote_data.get_min_load()
+        self._max_load = remote_data.get_max_load()
+        self._last_load = remote_data.get_last_load()
 
-	def get_last_load(self):
-		return self._last_load
-
-
-	def add_status_listener(self, listener):
-		self._status_listener.append(listener)
-
-	def remove_status_listener(self, listener):
-		self._status_listener.remove(listener)
-
-
-
-	def process_cpu_status_message(self, remote_data):
-		self._min_load = remote_data.get_min_load()
-		self._max_load = remote_data.get_max_load()
-		self._last_load = remote_data.get_last_load()
-
-		for listener in self._status_listener:
-			listener.cpu_status_changed(self)
+        for listener in self._status_listener:
+            listener.cpu_status_changed(self)
 
 
 """
--- Robot/Device/Protocol/Stream_cpuStatistics.py
+++ Robot/Device/Protocol/Stream_cpuStatistics.py
@@ -2,30 +2,31 @@ from RoboControl.Com.Remote.Parameter.RemoteParameterUint8 import RemoteParamete
 from RoboControl.Com.Remote.Parameter.RemoteParameterUint32 import RemoteParameterUint32
 from RoboControl.Com.Remote.RemoteStream import RemoteStream
 
-INDEX_ACTUAL_CPU_LOAD 	= 0
-INDEX_MIN_CPU_LOAD 		= 1
-INDEX_MAX_CPU_LOAD		= 2
+INDEX_ACTUAL_CPU_LOAD = 0
+INDEX_MIN_CPU_LOAD = 1
+INDEX_MAX_CPU_LOAD = 2
+
+
 class Stream_cpuStatistics(RemoteStream):
-	
-	def __init__(self, id):
-		super().__init__(id,"cpuStatus", "status of the cpu containing values for min max and last cycle duration")
 
-		self._parameter_list.append(RemoteParameterUint8("min","min load of the device cpu"))
-		self._parameter_list.append(RemoteParameterUint8("max","max load of the device cpu"))
-		self._parameter_list.append(RemoteParameterUint8("actual","actual load of the device cpu"))
-		self._parameter_list.append(RemoteParameterUint8("status","actual status of the device cpu"))
-		
-		self.set_id(id)
+    def __init__(self, id):
+        super().__init__(id, "cpuStatus", "status of the cpu containing values for min max and last cycle duration")
 
-	def get_last_load(self):
-		return self._parameter_list[INDEX_ACTUAL_CPU_LOAD].get_value()
+        self._parameter_list.append(RemoteParameterUint8("min", "min load of the device cpu"))
+        self._parameter_list.append(RemoteParameterUint8("max", "max load of the device cpu"))
+        self._parameter_list.append(RemoteParameterUint8("actual", "actual load of the device cpu"))
+        self._parameter_list.append(RemoteParameterUint8("status", "actual status of the device cpu"))
 
-	def get_min_load(self):
-		return self._parameter_list[INDEX_MIN_CPU_LOAD].get_value()
+        self.set_id(id)
 
-	def get_max_load(self):
-		return self._parameter_list[INDEX_MAX_CPU_LOAD].get_value()
+    def get_last_load(self):
+        return self._parameter_list[INDEX_ACTUAL_CPU_LOAD].get_value()
 
+    def get_min_load(self):
+        return self._parameter_list[INDEX_MIN_CPU_LOAD].get_value()
+
+    def get_max_load(self):
+        return self._parameter_list[INDEX_MAX_CPU_LOAD].get_value()
 
 
 """

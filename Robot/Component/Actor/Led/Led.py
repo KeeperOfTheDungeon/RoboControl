eb8236143a35adb8833999e006d7e8e29e257d7c
ermu1013 <ermu1013@gmail.com>
2023-09-17 22:22:54 +0200

    conflict: implement various java comments

--- Robot/Component/Actor/Led/Led.py
+++ Robot/Component/Actor/Led/Led.py
@@ -5,24 +5,19 @@ from RoboControl.Robot.Value.BrightnessValue import BrightnessValue
 
 
 class Led(Actor):
-
     def __init__(self, meta_data):
         super().__init__(meta_data)
         self._brightness_value = BrightnessValue(meta_data)
-        protocol = meta_data["protocol"]
-        self._cmd_set_led_brightness = protocol["cmd_setBrightness"]
+        self._component_protocol = meta_data.get("protocol")
 
-    def remote_set_brightness(self, brightness):
-        cmd = Cmd_setLedBrightness.get_command(self._cmd_set_led_brightness, self._local_id, brightness)
-        self.send_data(cmd)
+    def remote_set_brightness(self, brightness: float) -> bool:
+        if self._component_protocol is None:
+            return False
+        cmd = Cmd_setLedBrightness.get_command(self._component_protocol["cmd_setBrightness"], self._local_id, brightness )
+        return self.send_data(cmd)
 
-    """
-    def remote_send_msg_brightness(self, brightness):
-        cmd = Msg_setLedBrightness.get_command(self._cmd_set_led_brightness, self._local_id, brightness )
-        self.send_data(cmd)
-    """
-
-    def remote_get_value(self):
-        cmd = Cmd_getLedBrightness.get_command(self._componentProtocol.cmd_get_value_id, self._local_id)
-        cmd.set_brightness
-        self.send_data(cmd)
+    def remote_get_value(self) -> bool:
+        if self._component_protocol is None:
+            return False
+        cmd = Cmd_getLedBrightness.get_command(self._component_protocol["cmd_getValue"], self._local_id)
+        return self.send_data(cmd)
--- Robot/Device/Protocol/Msg_pingResponse.py
+++ Robot/Device/Protocol/Msg_pingResponse.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from RoboControl.Com.Remote.RemoteMessage import RemoteMessage
 from RoboControl.Com.Remote.Parameter.RemoteParameterUint8 import RemoteParameterUint8
 from RoboControl.Robot.Device.Protocol import DeviceProtocol
@@ -6,15 +8,22 @@ INDEX_TTL = 0
 
 
 class Msg_pingResponse(RemoteMessage):
+    _parameter_list: List[RemoteParameterUint8]
 
     def __init__(self, id: int = DeviceProtocol.MSG_PING_RESPONSE):
         super().__init__(id, "msgpingResponse", "response to a ping command")
         self._ttl_index = 0
         self._parameter_list.append(RemoteParameterUint8("ttl", "time to live"))
 
-    def get_command(id):
-        cmd = Msg_pingResponse()
-        return (cmd)
+    @staticmethod
+    def get_command(id: int, ttl: int = None) -> "Msg_pingResponse":
+        res = Msg_pingResponse(id)
+        if ttl is not None:
+            res.set_data(ttl)
+        return res
 
     def get_ttl(self):
         return self._parameter_list[INDEX_TTL].get_value()
+
+    def set_data(self, node_type: int) -> None:
+        self._parameter_list[INDEX_TTL].set_value(node_type)

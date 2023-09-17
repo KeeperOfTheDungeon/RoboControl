8bf3dbf3f691af99c14ef0261cc4360ce0d02c0b
ermu1013 <ermu1013@gmail.com>
2023-09-17 22:06:35 +0200

    conflict: make type and type_name for commands static

--- Com/Remote/RemoteMessage.py
+++ Com/Remote/RemoteMessage.py
@@ -4,6 +4,7 @@ from RoboControl.Com.Remote.RemoteDataPacket import RemoteDataPacket
 
 
 class RemoteMessage(RemoteData):
+    _type_name: str = "message"
 
     def get_data_packet(self) -> RemoteDataPacket:
         data_packet = RemoteMessageDataPacket(self.get_destination_address(), self.get_source_address(), self.get_id())

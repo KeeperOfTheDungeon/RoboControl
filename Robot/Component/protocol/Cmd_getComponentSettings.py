ff1ab7af4508cc7290cd5e61511ef795ac3e5eb2
ermu1013 <ermu1013@gmail.com>
2023-10-23 11:27:30 +0200

    WIP fix parameter indexes should be consistent with the java project

--- Robot/Component/protocol/Cmd_getComponentSettings.py
+++ Robot/Component/protocol/Cmd_getComponentSettings.py
@@ -3,20 +3,21 @@ from typing import List
 from RoboControl.Com.Remote.RemoteCommand import RemoteCommand
 from RoboControl.Com.Remote.Parameter.RemoteParameterUint8 import RemoteParameterUint8
 
+INDEX_TYPE = 0
+
 
 class Cmd_getComponentSettings(RemoteCommand):
     _parameter_list: List[RemoteParameterUint8]
-    _index = 0
 
     def __init__(self, id):
         super().__init__(id, "getComponentSettings", "get components active settings")
         self._parameter_list.append(RemoteParameterUint8("index", "component index"))
 
     def set_index(self, index):
-        self._parameter_list[self._index].set_value(index)
+        self._parameter_list[INDEX_TYPE].set_value(index)
 
     def get_index(self):
-        return self._parameter_list[self._index].get_value()
+        return self._parameter_list[INDEX_TYPE].get_value()
 
     @staticmethod
     def get_command(id, local_id):

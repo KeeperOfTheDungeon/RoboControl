21e43b976dda1c9637909fd66d35e90039cd6710
Oktavian Gniot <tavin@tavin.de>
2023-11-07 00:31:36 +0100

    moved LuxValue to LuxProtocol, added Light Sensor Protocol
    Signed-off-by: Oktavian Gniot <tavin@tavin.de>

--- Robot/Component/Sensor/LightSensorProtocol.py
+++ Robot/Component/Sensor/LightSensorProtocol.py
@@ -1,10 +1,10 @@
 from RoboControl.Com.RemoteData import RemoteCommand, RemoteMessage, RemoteStream
-from RoboControl.Com.RemoteParameter import RemoteParameter, RemoteParameterUint8
+from RoboControl.Com.RemoteParameter import RemoteParameter,  RemoteParameterUint8
 
 
 INDEX_SENSOR = 0
 
-class Cmd_getLux(RemoteCommand):
+class Cmd_getLight(RemoteCommand):
 
 
     def __init__(self, id: int):
@@ -16,23 +16,23 @@ class Cmd_getLux(RemoteCommand):
 
     @staticmethod
     def get_command(id: int, local_id: int):
-        cmd = Cmd_getLux(id)
+        cmd = Cmd_getLight(id)
         cmd.set_index(local_id)
         return cmd
     
 
-class Msg_lux(RemoteMessage):
+class Msg_light(RemoteMessage):
 
     INDEX_LUX = 1
 
     def __init__(self, id: int):
-        super().__init__(id,  "actual lux value measured by a light sensor")
+        super().__init__(id,  "value measured by a light sensor")
         self._parameter_list.append(RemoteParameterUint8("index", "sensor index"))
-        self._parameter_list.append(RemoteParameterLuxValue("distance", "light value in lux"))
+        self._parameter_list.append(RemoteParameterLightValue("distance", "light value"))
 
     @staticmethod
     def get_command(id, index, distance):
-        cmd = Msg_lux(id)
+        cmd = Msg_light(id)
         if None not in [index, distance]:
             cmd.set_data(index, distance)
         return cmd
@@ -41,21 +41,21 @@ class Msg_lux(RemoteMessage):
         return self._parameter_list[INDEX_SENSOR].get_value()
 
     def get_lux_value(self) -> float:
-        return self._parameter_list[Msg_lux.INDEX_LUX].get_value()
+        return self._parameter_list[Msg_light.INDEX_LUX].get_value()
 
     def set_data(self, index: int, value: float) -> None:
         self._parameter_list[INDEX_SENSOR].set_value(index)
-        self._parameter_list[Msg_lux.INDEX_LUX].set_value(value)
+        self._parameter_list[Msg_light.INDEX_LUX].set_value(value)
 
 
-class Stream_lux(RemoteStream):
+class Stream_light(RemoteStream):
 
     def __init__(self, id: int):
         super().__init__(id,  "actual measured light in lux")
 
     @staticmethod
     def get_command(id: int, values):
-        cmd = Stream_lux(id)
+        cmd = Stream_light(id)
         if values:
             cmd.set_data(values)
         return cmd
@@ -72,18 +72,18 @@ class Stream_lux(RemoteStream):
         return self._parameter_list[index].get_value()
 
     def make_parameter(self, index: int):
-        return RemoteParameterLuxValue(f"lux {index}", f"measured lux value from sensor {index}")
+        return RemoteParameterLightValue(f"lux {index}", f"measured lux value from sensor {index}")
 
     def parse_data_packet_data(self, data_packet):
         return self.parse_data_packet_data_dynamic(data_packet)
 
 
-class RemoteParameterLuxValue(RemoteParameter):
-
-    BYTE_SIZE = 3
-
+class RemoteParameterLightValue(RemoteParameter):
+    
+    BYTE_SIZE = 2
+    
     def __init__(self, name, description):
-        super().__init__(name, description, RemoteParameterLuxValue.BYTE_SIZE)
+        super().__init__(name, description, RemoteParameterLightValue.BYTE_SIZE)
         self._value = 0.0
 
     def put_data(self, data_buffer: list) -> None:
@@ -91,19 +91,19 @@ class RemoteParameterLuxValue(RemoteParameter):
         # data_buffer.put(high)
         # low = self._value & 0xffff
         # data_buffer.put_char(low)
-        data_buffer.append(self._value + 0x800000)
+        value = int(value * 65535)
+        data_buffer.append((value & 0xff00) >> 8)
+        data_buffer.append(value & 0xff)
+
 
     def parse_from_buffer(self, data_buffer: bytearray, index: int) -> int:
-        # if self._value > 0xffffff:
-        #     self._value = 0xffffff
-        value = data_buffer[index] << 16
-        value |= data_buffer[index + 1] << 8
-        value |= data_buffer[index + 2]
+        value |= data_buffer[index] << 8
+        value |= data_buffer[index + 1]
 
-        self._value = value * 0.25
+        self._value = value * 65535
         return self._byte_size
 
     def get_as_string(self, description: bool) -> str:
         if description:
-            return self._name + "=" + str(self._value) + "lux"
+            return self._name + "=" + str(self._value) + "light"
         return str(self._value)
